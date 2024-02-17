767f5d552efab503259c715887c8fa61db7ad014
BasharAbuLeil <bashar.ab@campus.technion.ac.il>
2024-02-17 14:18:25 +0000

    90% of the tests has passed

--- hw1/HW1-student-files/ex3.asm
+++ hw1/HW1-student-files/ex3.asm
@@ -6,7 +6,7 @@ _start:
     movb $1,(bool) #  we are assuming that there is a valid partition
     movl $0,%esi # %rsi will save the index of the current element in the source array 
     movq $up_array,%rbx
-    movq $0,%r8d # r8d saves the size of the inc array
+    movl $0,%r8d # r8d saves the size of the inc array
     movq $down_array,%rcx
     movl $0,%r9d  # r9d saves the size of the dec array
     movq $source_array,%rax
@@ -14,91 +14,92 @@ _start:
 
 .loopStart:
     test %r8d,%r8d
-    jz emptyIncArray
+    jz .emptyIncArray
     test %r9d,%r9d
-    jz emptyDecArray
+    jz .emptyDecArray
     # in this line we have validated that the dec&inc array are not empty (Normal code)
     lea -4(%rbx),%r10 # have the adress of the last element of inc array
-    lea -4(rcx),%r11  # have the adress of the last element of dec array
+    lea -4(%rcx),%r11  # have the adress of the last element of dec array
     movl (%r10),%r10d # inc
     movl (%r11),%r11d # dec
     cmpl (%rax), %r10d # inc < (rax)
-    jl  incLastSmallerThanCurrentIndex
+    jl  .incLastSmallerThanCurrentIndex
     # in this line arr[i]<inc  %r10>(%rax)
     cmpl (%rax) ,%r11d 
-    jl setAnswerIsWrong
+    jl .setAnswerIsWrong
     # (%rax) should put be put in dec array
-    jmp insertInDec
+    jmp .insertInDec
 
 .loopCheck:
     lea 4(%rax),%rax # ptr++
     inc %esi 
     cmpl (size),%esi
-    jb loopStart
-    jmp  exit
+    jb .loopStart
+    jmp  .exit
 
 .setAnswerIsWrong:
     movb $0,(bool)
-    jmp exit
+    jmp .exit
 
 .incLastSmallerThanCurrentIndex:
     cmpl (%rax),%r11d
-    jl insertInInc
+    jl .insertInInc
     movl %esi, %r12d
     inc %r12d
     cmpl (size),%r12d
-    jae insertInInc
+    jae .insertInInc
     # we are in the bounds of the source array 
     lea 4(%rax) ,%r10 
     movl (%r10),%r10d # getting the next element in source array arr[i+1]
     cmpl (%rax),%r10d
-    jl insertInInc
-    jmp insertInDec
+    jl .insertInInc
+    jmp .insertInDec
 
 .insertInDec:
     movl (%rax),%r10d
     movl %r10d , (%rcx)
     lea 4(%rcx) , %rcx
     inc %r9d
-    jmp loopCheck
+    jmp .loopCheck
 
 .insertInInc:
     movl (%rax),%r10d
     movl %r10d, (%rbx)
     lea 4(%rbx),%rbx
     inc %r8d
-    jmp loopCheck
+    jmp .loopCheck
 
 .bothAreEmpty:
     #  check this function again .
-    movl (rax),%r10d
+    movl (%rax),%r10d
     # in the next section we're gonna check if the we are in the boundaries
     movl %esi, %r12d
     inc %r12d
     cmpl (size),%r12d
-    jae insertInInc
+    jae .insertInInc
     # we valdiated that we are in valid boundaries of the source_array
     lea 4(%rax),%r11
     movl (%r11),%r11d
     cmpl %r11d,%r10d
-    jl insertInInc
-    jmp insertInDec
+    jl .insertInInc
+    jmp .insertInDec
 
 .emptyIncArray:
     test %r9d,%r9d
-    jz bothAreEmpty
+    jz .bothAreEmpty
     # in this line inc array is empty but dec ain't
-    movl (%rcx)%r10d
+    movl (%rcx),%r10d
     cmpl (%rax),%r10d
-    jl insertInDec   
-    jmp insertInInc
+    jl .insertInDec   
+    jmp .insertInInc
 
 .emptyDecArray:
     # inc cannot be empty
     movl (%rbx),%r10d # set the inc last val in %r10d
     cmpl (%rax),%r10d 
-    jl insertInInc 
-    jmp insertInDec
+    jl .insertInInc 
+    jmp .insertInDec
 
 .exit:
     xor %rax,%rax # stam instruction
+    
