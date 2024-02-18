ed6e1d32f1ba0b552feb65a68572e06335c67258
BasharAbuLeil <bashar.ab@campus.technion.ac.il>
2024-02-18 11:50:56 +0000

    ex2 first try  checking if there are slef loops

--- hw1/HW1-student-files/ex2.asm
+++ hw1/HW1-student-files/ex2.asm
@@ -2,4 +2,47 @@
 
 .section .text
 _start:
-#your code here
+    movq $vertices,%rax 
+    movb $0,(circle)
+
+.whileSelfLoopCheck:
+    movq (%rax),%rbx # rbx gets the node which is an array of nodes
+    jmp .checkSelfLoop
+
+.endWhileCheck:
+    lea 8(%rax),%rax
+    movq (%rax),%rdx
+    test %rdx,%rdx 
+    jz .noSelfLoops
+    jmp .whileSelfLoopCheck
+
+
+.checkSelfLoop:
+    # rbx has the node
+    movq (%rbx),%rcx
+    test %rcx,%rcx
+    jz .endWhileCheck
+    cmpq (%rax),%rcx
+    jz .cyclicGraph
+    lea 8(%rbx),%rbx
+    jmp .checkSelfLoop
+    
+.noSelfLoops:
+    # at this line we have validated that there is no self loops (normalCode)
+    movq $vertices,%rax 
+    
+
+
+
+
+
+
+.cyclicGraph:
+    movb $1,(circle)
+    jmp .exit
+
+
+
+.exit:
+    xor %rax,%rax
+
